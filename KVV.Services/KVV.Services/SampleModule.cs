3ff95a03014237ebc2ab356cb27bd05b27dd7f0e
Erik Lenaerts <erik.lenaerts@line20.be>
2012-07-17 17:20:54 +0200

    Initial commit

---
+++ KVV.Services/KVV.Services/SampleModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KVV.Services
+{
+    public class SampleModule : Nancy.NancyModule
+    {
+        public SampleModule()
+        {
+            Get["/"] = _ => "Hello World!";
+        }
+    }
+}
